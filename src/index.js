4b9e9440ab26b50616ce9b3d7248c32ccaa87e91
Kieu Anh (Katie) Pham <phamkieuanh33@gmail.com>
2025-02-27 16:01:30 +1100

    Added basic form handling and added Typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: "this is a poem",
+    autoStart: true,
+    delay: 60,
+    cursor: "🌸",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
